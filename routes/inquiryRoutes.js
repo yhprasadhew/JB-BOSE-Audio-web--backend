7754eb295c527ac07f244fe8c8db9b87d76415dc
yhprasad <yhprasadhew@gmail.com>
2026-06-21 05:48:37 +0530

    backend completed

--- routes/inquiryRoutes.js
+++ routes/inquiryRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addInquiry, deleteInquiry, getInquiries } from "../controllers/inquiryController.js";
+import { addInquiry, deleteInquiry, getInquiries, updateInquiry } from "../controllers/inquiryController.js";
 
 
 const inquiryRouter = express.Router();
@@ -7,6 +7,7 @@ const inquiryRouter = express.Router();
 inquiryRouter.post("/",addInquiry);
 inquiryRouter.get("/",getInquiries);
 inquiryRouter.delete("/:id",deleteInquiry);
+inquiryRouter.put("/:id",updateInquiry);
 
 
 
